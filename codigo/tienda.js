e7d3ab505897f154009400058f3b3a8fa3c9df26
tonyponyy <58556009+tonyponyy@users.noreply.github.com>
2021-05-30 18:46:21 +0200

    Actualización menor
    las funciones en flecha de drag and drop ahora solo tienen una linea.

--- codigo/tienda.js
+++ codigo/tienda.js
@@ -128,14 +128,10 @@ var recuperar_cesta=()=>{
   // DRAG AND DROP :
 
   // se añade esta parte de codigo para que funcione todo el tema del drag and drop
-  document.addEventListener("dragover", dragover=(event)=> {
-    event.preventDefault();
-  });
+  document.addEventListener("dragover", dragover=(event)=> event.preventDefault());
 
 //creamos un listener para cuando se empieze a arrastrar guarda el ID del div en setData como "plain";
-  document.addEventListener("dragstart", dragstart=(event)=> {
-    event.dataTransfer.setData("plain",event.target.id);  
-  }, false);
+  document.addEventListener("dragstart", dragstart=(event)=> event.dataTransfer.setData("plain",event.target.id));
 
 //  recuperamos la información con getData y si el id en el que vamos a soltar es "cesto", se ejecuta la funcion cesta(id) donde el id
 //  es el id del div, que coincide con el id del producto. 
